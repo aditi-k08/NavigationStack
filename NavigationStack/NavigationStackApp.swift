5a87c6192ea54012899b7cc3ec14a6e978c68602
Aditi Kesarwani <aditi_k08@outlook.com>
2025-07-24 09:30:04 -0700

    Initial Commit

---
+++ NavigationStack/NavigationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackApp.swift
+//  NavigationStack
+//
+//  Created by Scholar on 7/24/25.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
